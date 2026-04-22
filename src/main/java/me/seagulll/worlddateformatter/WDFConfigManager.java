0dc35ea70e0237188b26bc3bdcc12c2c4f086849
Seagulll <mckamome@gmail.com>
2026-04-22 18:14:30 +0900

    設定の即時反映機能追加

--- src/main/java/me/seagulll/worlddateformatter/WDFConfigManager.java
+++ src/main/java/me/seagulll/worlddateformatter/WDFConfigManager.java
@@ -1,39 +1,47 @@
-package me.seagulll.worlddateformatter;
+    package me.seagulll.worlddateformatter;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import net.fabricmc.loader.api.FabricLoader;
+    import com.google.gson.Gson;
+    import com.google.gson.GsonBuilder;
+    import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+    import java.io.File;
+    import java.io.FileReader;
+    import java.io.FileWriter;
 
-public class WDFConfigManager {
-    private static final File FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), WDF.MOD_ID + ".json");
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    public static WDFConfig config = new WDFConfig();
+    public class WDFConfigManager {
+        private static final File FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), WDF.MOD_ID + ".json");
+        private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+        private static WDFConfig config = new WDFConfig();
+        private static long timestamp = 0;
 
-    public static void load() {
-        try {
+        public static WDFConfig load() {
             if (FILE.exists()) {
-                FileReader reader = new FileReader(FILE);
-                config = GSON.fromJson(reader, WDFConfig.class);
-                reader.close();
-            } else {
+                try (FileReader reader = new FileReader(FILE)) {
+                    long lastModified = FILE.lastModified();
+                    if (timestamp == 0 || timestamp != lastModified) {
+                        WDFConfig newConfig = GSON.fromJson(reader, WDFConfig.class);
+                        if (newConfig != null) {
+                            config = newConfig;
+                        }
+                        timestamp = lastModified;
+                    }
+                }
+                catch (Exception e) {
+                    WDF.LOGGER.error("Failed to load config", e);
+                }
+            }
+            else {
                 save();
             }
-        } catch (Exception e) {
-            WDF.LOGGER.error("Failed to load config", e);
+            return config;
         }
-    }
 
-    public static void save() {
-        try {
-            FileWriter writer = new FileWriter(FILE);
-            GSON.toJson(config, writer);
-            writer.close();
-        } catch (Exception e) {
-            WDF.LOGGER.error("Failed to save config", e);
+        public static void save() {
+            try (FileWriter writer = new FileWriter(FILE)) {
+                GSON.toJson(config, writer);
+                timestamp = FILE.lastModified();
+            } catch (Exception e) {
+                WDF.LOGGER.error("Failed to save config", e);
+            }
         }
     }
-}
